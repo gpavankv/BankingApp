3fe0dd8f99149dca7ad25c2aaf79d1ea383aaea8
gpavankv <gpavankv@gmail.com>
2017-12-12 15:41:17 -0800

    added Data binding examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,24 +8,33 @@ export class AppComponent implements OnInit {
   title = 'First Angular App';
   btnValue = true;
   classBtnValue = '';
+  clickEventValue = true;
 
-  buttonToggle(){
-    if(this.btnValue == true){
+  buttonToggle() {
+    if (this.btnValue === true) {
       this.classBtnValue = 'btn btn-primary';
       this.btnValue  = false;
-      console.log("prim");
-    }
-    else{
+      console.log('prim');
+    } else {
       this.classBtnValue = 'btn btn-danger';
       this.btnValue = true;
-      console.log("dang");
+      console.log('dang');
+    }
+  }
+
+  clickEvent() {
+    if (this.clickEventValue === true) {
+      document.getElementById('eventBind').innerHTML = 'This is a response based on click event!';
+      this.clickEventValue = false;
+    } else {
+      document.getElementById('eventBind').innerHTML = '';
+      this.clickEventValue = true;
     }
   }
 
   ngOnInit() {
-    setInterval(()=> {
-      this.buttonToggle(); },2000);
+    setInterval(() => {
+      this.buttonToggle(); }, 2000);
   }
-
 }
 
