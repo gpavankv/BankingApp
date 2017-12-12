1ef593ed7fee5d9987eb74a67531dd07db6b0c52
gpavankv <gpavankv@gmail.com>
2017-12-11 19:17:38 -0800

    interpolation and Property binging examples in appComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'First Angular App';
+  btnValue = true;
+  classBtnValue = '';
+
+  buttonToggle(){
+    if(this.btnValue == true){
+      this.classBtnValue = 'btn btn-primary';
+      this.btnValue  = false;
+      console.log("prim");
+    }
+    else{
+      this.classBtnValue = 'btn btn-danger';
+      this.btnValue = true;
+      console.log("dang");
+    }
+  }
+
+  ngOnInit() {
+    setInterval(()=> {
+      this.buttonToggle(); },2000);
+  }
+
 }
+
